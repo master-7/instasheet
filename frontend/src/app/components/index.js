28410557a7334854a068e3010c92e8b9321dfa3b
Nick Masters <master-7@inbox.ru>
2017-07-01 15:31:40 +0300

    Structure refactor

--- frontend/src/app/components/index.js
+++ frontend/src/app/components/index.js
@@ -1,6 +1,1 @@
-export {default as Breadcrumbs} from './Breadcrumbs'
-export {default as BreadcrumbsItem} from './Breadcrumbs/Item'
-export {default as Footer} from './Footer'
-export {default as Layout} from './Layout'
-export {default as Menu} from './Menu'
-export {default as MenuItem} from './Menu/Item'
+export {default as Button} from './Button'
