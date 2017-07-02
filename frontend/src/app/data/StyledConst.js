fe4e68d4f9b1015f801271b43af66b3085e18b4b
Nick Masters <master-7@inbox.ru>
2017-07-02 22:48:31 +0300

    The common styles const

---
+++ frontend/src/app/data/StyledConst.js
@@ -0,0 +1,6 @@
+export const Panels = {
+    fontFamily: 'FreeSans,Arimo,"Droid Sans",Helvetica,Arial,sans-serif',
+    fontColor: '#fff',
+    background: 'rgb(37, 42, 58)',
+    elementIsActiveBackground: 'rgb(55, 60, 90)'
+};
